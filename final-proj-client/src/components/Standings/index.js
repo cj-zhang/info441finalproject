a14d9d3be887e3f12e58a69a4196a9c0d475ea90
Joseph Zhang <cjzhang@uw.edu>
2019-03-19 00:58:20 -0700

    working on standings

--- final-proj-client/src/components/Standings/index.js
+++ final-proj-client/src/components/Standings/index.js
@@ -5,9 +5,18 @@ import "./style.css";
 export default class Standings extends Component {
     render() {
         return (
-            <h1>
-                Standings
-            </h1>
+            <div>
+                <h1 className="text-center my-5">
+                    Standings
+                </h1>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Don't Park On the Grass</h5>
+                        <p className="mb-1 py-0 text-secondary">December 19th-21st 2018</p>
+                        <p className="card-text"><span className="text-success">WON 2-0</span> against <strong>Jo96</strong></p>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
--- final-proj-client/src/components/Tournaments/index.js
+++ final-proj-client/src/components/Tournaments/index.js
@@ -29,65 +29,65 @@ export default class Tournaments extends Component {
                 </h1>
                 <div className="d-flex mb-5 justify-content-center"> 
                     <div className="card mx-4" style={cardStyle}>
-                        <img class="card-img-top" src="https://smashgg.imgix.net/images/tournament/143919/image-b0e64c115be8f6274573d49974ff376f.jpg?auto=compress,format&w=300" alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="mb-1 card-title">Twitch Rivals: Showdown</h5>
+                        <img className="card-img-top" src="https://smashgg.imgix.net/images/tournament/143919/image-b0e64c115be8f6274573d49974ff376f.jpg?auto=compress,format&w=300" alt="Card image cap"/>
+                        <div className="card-body">
+                            <h5 className="mb-1 card-title">Twitch Rivals: Showdown</h5>
                             <p className="mb-1 py-0 text-secondary">March 19th-21st 2019</p>
-                            <p class="card-text">The Twitch Rivals: The Division 2 Showdown is an online competition, split between Europe and North America, featuring 40 invited streamers (20 per region). </p>
-                            <a href="#" class="btn btn-primary mr-3">Details</a>
-                            <a href="#" class="btn btn-success">Register</a>
+                            <p className="card-text">The Twitch Rivals: The Division 2 Showdown is an online competition, split between Europe and North America, featuring 40 invited streamers (20 per region). </p>
+                            <a href="#" className="btn btn-primary mr-3">Details</a>
+                            <a href="#" className="btn btn-success">Register</a>
                         </div>
                     </div>
                     <div className="card mx-4" style={cardStyle}>
-                        <img class="card-img-top" src="https://smashgg.imgix.net/images/tournament/96984/image-9d8dd9be245d87fd520ec66cf5e4300d.png?auto=compress,format&w=300" alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="mb-1 card-title">Full Bloom 5</h5>
+                        <img className="card-img-top" src="https://smashgg.imgix.net/images/tournament/96984/image-9d8dd9be245d87fd520ec66cf5e4300d.png?auto=compress,format&w=300" alt="Card image cap"/>
+                        <div className="card-body">
+                            <h5 className="mb-1 card-title">Full Bloom 5</h5>
                             <p className="mb-1 py-0 text-secondary">March 19th-21st 2019</p>
-                            <p class="card-text">“Full Bloom 5” is a Melee/Smash Ultimate tournament held by Smash at IUB. Registration is mandatory and closes March 9th, 2019 at 11:59pm EDT. There will be no registration on site.</p>
-                            <a href="#" class="btn btn-primary mr-3">Details</a>
-                            <a href="#" class="btn btn-success">Register</a>
+                            <p className="card-text">“Full Bloom 5” is a Melee/Smash Ultimate tournament held by Smash at IUB. Registration is mandatory and closes March 9th, 2019 at 11:59pm EDT. There will be no registration on site.</p>
+                            <a href="#" className="btn btn-primary mr-3">Details</a>
+                            <a href="#" className="btn btn-success">Register</a>
                         </div>
                     </div>
                     <div className="card mx-4" style={cardStyle}>
-                        <img class="card-img-top" src="https://smashgg.imgix.net/images/tournament/143919/image-b0e64c115be8f6274573d49974ff376f.jpg?auto=compress,format&w=300" alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="mb-1 card-title">Twitch Rivals: Showdown</h5>
+                        <img className="card-img-top" src="https://smashgg.imgix.net/images/tournament/143919/image-b0e64c115be8f6274573d49974ff376f.jpg?auto=compress,format&w=300" alt="Card image cap"/>
+                        <div className="card-body">
+                            <h5 className="mb-1 card-title">Twitch Rivals: Showdown</h5>
                             <p className="mb-1 py-0 text-secondary">March 19th-21st 2019</p>
-                            <p class="card-text">The Twitch Rivals: The Division 2 Showdown is an online competition, split between Europe and North America, featuring 40 invited streamers (20 per region). </p>
-                            <a href="#" class="btn btn-primary mr-3">Details</a>
-                            <a href="#" class="btn btn-success">Register</a>
+                            <p className="card-text">The Twitch Rivals: The Division 2 Showdown is an online competition, split between Europe and North America, featuring 40 invited streamers (20 per region). </p>
+                            <a href="#" className="btn btn-primary mr-3">Details</a>
+                            <a href="#" className="btn btn-success">Register</a>
                         </div>
                     </div>
                 </div>
                 <div className="d-flex mb-5 justify-content-center"> 
                     <div className="card mx-4" style={cardStyle}>
-                        <img class="card-img-top" src="https://smashgg.imgix.net/images/tournament/143919/image-b0e64c115be8f6274573d49974ff376f.jpg?auto=compress,format&w=300" alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="mb-1 card-title">Twitch Rivals: Showdown</h5>
+                        <img className="card-img-top" src="https://smashgg.imgix.net/images/tournament/143919/image-b0e64c115be8f6274573d49974ff376f.jpg?auto=compress,format&w=300" alt="Card image cap"/>
+                        <div className="card-body">
+                            <h5 className="mb-1 card-title">Twitch Rivals: Showdown</h5>
                             <p className="mb-1 py-0 text-secondary">March 19th-21st 2019</p>
-                            <p class="card-text">The Twitch Rivals: The Division 2 Showdown is an online competition, split between Europe and North America, featuring 40 invited streamers (20 per region). </p>
-                            <a href="#" class="btn btn-primary mr-3">Details</a>
-                            <a href="#" class="btn btn-success">Register</a>
+                            <p className="card-text">The Twitch Rivals: The Division 2 Showdown is an online competition, split between Europe and North America, featuring 40 invited streamers (20 per region). </p>
+                            <a href="#" className="btn btn-primary mr-3">Details</a>
+                            <a href="#" className="btn btn-success">Register</a>
                         </div>
                     </div>
                     <div className="card mx-4" style={cardStyle}>
-                        <img class="card-img-top" src="https://smashgg.imgix.net/images/tournament/143919/image-b0e64c115be8f6274573d49974ff376f.jpg?auto=compress,format&w=300" alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="mb-1 card-title">Twitch Rivals: Showdown</h5>
+                        <img className="card-img-top" src="https://smashgg.imgix.net/images/tournament/143919/image-b0e64c115be8f6274573d49974ff376f.jpg?auto=compress,format&w=300" alt="Card image cap"/>
+                        <div className="card-body">
+                            <h5 className="mb-1 card-title">Twitch Rivals: Showdown</h5>
                             <p className="mb-1 py-0 text-secondary">March 19th-21st 2019</p>
-                            <p class="card-text">The Twitch Rivals: The Division 2 Showdown is an online competition, split between Europe and North America, featuring 40 invited streamers (20 per region). </p>
-                            <a href="#" class="btn btn-primary mr-3">Details</a>
-                            <a href="#" class="btn btn-success">Register</a>
+                            <p className="card-text">The Twitch Rivals: The Division 2 Showdown is an online competition, split between Europe and North America, featuring 40 invited streamers (20 per region). </p>
+                            <a href="#" className="btn btn-primary mr-3">Details</a>
+                            <a href="#" className="btn btn-success">Register</a>
                         </div>
                     </div>
                     <div className="card mx-4" style={cardStyle}>
-                        <img class="card-img-top" src="https://smashgg.imgix.net/images/tournament/143919/image-b0e64c115be8f6274573d49974ff376f.jpg?auto=compress,format&w=300" alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="mb-1 card-title">Twitch Rivals: Showdown</h5>
+                        <img className="card-img-top" src="https://smashgg.imgix.net/images/tournament/143919/image-b0e64c115be8f6274573d49974ff376f.jpg?auto=compress,format&w=300" alt="Card image cap"/>
+                        <div className="card-body">
+                            <h5 className="mb-1 card-title">Twitch Rivals: Showdown</h5>
                             <p className="mb-1 py-0 text-secondary">March 19th-21st 2019</p>
-                            <p class="card-text">The Twitch Rivals: The Division 2 Showdown is an online competition, split between Europe and North America, featuring 40 invited streamers (20 per region). </p>
-                            <a href="#" class="btn btn-primary mr-3">Details</a>
-                            <a href="#" class="btn btn-success">Register</a>
+                            <p className="card-text">The Twitch Rivals: The Division 2 Showdown is an online competition, split between Europe and North America, featuring 40 invited streamers (20 per region). </p>
+                            <a href="#" className="btn btn-primary mr-3">Details</a>
+                            <a href="#" className="btn btn-success">Register</a>
                         </div>
                     </div>
                 </div>

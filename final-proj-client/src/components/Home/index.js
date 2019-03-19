f68e157702989b345e6604eedb8c94e48048fc2d
Sunwoo Kang <sunwookang@Sunwoos-MacBook-Pro.local>
2019-03-19 02:42:35 -0700

    jo go the signin and signup to work

--- final-proj-client/src/components/Home/index.js
+++ final-proj-client/src/components/Home/index.js
@@ -3,65 +3,21 @@ import jumbotron from "react-bootstrap"
 import { Row, Col, Container } from 'react-bootstrap';
 import "./style.css";
 import { Link } from "react-router-dom";
+import Tournaments from "../Tournaments";
 
 
 export default class Home extends Component {
     render() {
-        const jumboStyle = {
-            backgroundImage: "url(./img/smash_banner_background.png)",
-        };
         return (
             <div>
-                <Row className="jumbotron" style={jumboStyle}>
+                <Row className="mainjumbotron">
                     <Col className="container title-container">
                         <h1 className="smash-title">Smash.qq</h1>
                         <h2 className="title-text">Find your local Super Smash Bros. Ultimate tournaments</h2>
                         <h3 className="signuplink">Sign Up <Link to={"/login"} className="now">Now</Link>!</h3>
                     </Col>
                 </Row>
-                <div className="container-fluid bg-3 text-center">
-                    <h3>Local tournaments</h3>
-                    <Row className="row">
-                        <Col sm={3} className="col-sm-3">
-                            <p>Some text..</p>
-                            <img src="https://placehold.it/150x80?text=IMAGE" className="img-responsive" style={{width: '100%'}} alt="Image"></img>
-                        </Col>
-                        <Col sm={3} className="col-sm-3">
-                            <p>Some text..</p>
-                            <img src="https://placehold.it/150x80?text=IMAGE" className="img-responsive" style={{width: '100%'}} alt="Image"></img>
-                        </Col>
-                        <Col sm={3} className="col-sm-3">
-                            <p>Some text..</p>
-                            <img src="https://placehold.it/150x80?text=IMAGE" className="img-responsive" style={{width: '100%'}} alt="Image"></img>
-                        </Col>
-                        <Col sm={3} className="col-sm-3">
-                            <p>Some text..</p>
-                            <img src="https://placehold.it/150x80?text=IMAGE" className="img-responsive" style={{width: '100%'}} alt="Image"></img>
-                        </Col>
-
-                    </Row>
-                </div>
-
-                <div class="container-fluid bg-3 text-center">
-                    <div class="row">
-                        <div class="col-sm-3">
-                            <p>Some text..</p>
-                            <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style={{width: '100%'}} alt="Image"></img>
-                        </div>
-                        <div class="col-sm-3">
-                            <p>Some text..</p>
-                            <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style={{width: '100%'}} alt="Image"></img>
-                        </div>
-                        <div class="col-sm-3">
-                            <p>Some text..</p>
-                            <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style={{width: '100%'}} alt="Image"></img>
-                        </div>
-                        <div class="col-sm-3">
-                            <p>Some text..</p>
-                            <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style={{width: '100%'}} alt="Image"></img>
-                        </div>
-                    </div>
-                </div>
+                <Tournaments/>
                 <br></br>
                 <div id="root"></div>
             </div>

ed45c2ea17892bd7b9a4e8052a6eb067ba59ac11
Sunwoo Kang <sunwookang@Sunwoos-MacBook-Pro.local>
2019-03-18 20:26:55 -0700

    all skeleton pages are up

---
+++ final-proj-client/src/components/Standings/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./style.css";
+
+
+export default class Standings extends Component {
+    render() {
+        return (
+            <h1>
+                Standings
+            </h1>
+        );
+    }
+}

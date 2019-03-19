f7d42c67ecff95906f88ac9282ed44d9db444911
Joseph Zhang <cjzhang@uw.edu>
2019-03-18 23:14:09 -0700

    redeploying

--- final-proj-client/src/components/Tournaments/index.js
+++ final-proj-client/src/components/Tournaments/index.js
@@ -7,16 +7,9 @@ export default class Tournaments extends Component {
         // Default options are marked with *
         return fetch(url, {
             method: "GET", // *GET, POST, PUT, DELETE, etc.
-            mode: "cors", // no-cors, cors, *same-origin
+            mode: "no-cors", // no-cors, cors, *same-origin
             cache: "default", // *default, no-cache, reload, force-cache, only-if-cached
             credentials: "same-origin", // include, *same-origin, omit
-            headers: {
-                "Content-Type": "application/json",
-                // "Content-Type": "application/x-www-form-urlencoded",
-            },
-            redirect: "follow", // manual, *follow, error
-            referrer: "no-referrer", // no-referrer, *client
-            body: JSON.stringify(data), // body data type must match "Content-Type" header
         })
         .then(response => response.json()); // parses JSON response into native Javascript objects 
     }
@@ -26,7 +19,7 @@ export default class Tournaments extends Component {
     
 
     render() {
-        this.getTournaments("http://smash.chenjosephzhang.me/v1/tournaments")
+        this.getTournaments("https://smash.chenjosephzhang.me/v1/tournaments")
         .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
         .catch(error => console.error(error));
         return (
